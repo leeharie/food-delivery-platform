489d9b13f5b6b1389fa5e4d46a1cb8c9276b680d
Lahari <your_email@gmail.com>
2026-05-18 19:45:09 +0530

    feat: improve login and register frontend UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,62 @@
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f2f2f2"
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "30px",
+          borderRadius: "10px",
+          width: "300px",
+          boxShadow: "0px 0px 10px gray"
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "15px",
+            borderRadius: "5px"
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "15px",
+            borderRadius: "5px"
+          }}
+        />
+
+        <button
+          style={{
+            width: "100%",
+            padding: "10px",
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer"
+          }}
+        >
+          Login
+        </button>
+
+      </div>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,73 @@
 function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f2f2f2"
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "30px",
+          borderRadius: "10px",
+          width: "300px",
+          boxShadow: "0px 0px 10px gray"
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Register</h2>
+
+        <input
+          type="text"
+          placeholder="Enter Name"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "15px",
+            borderRadius: "5px"
+          }}
+        />
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "15px",
+            borderRadius: "5px"
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "15px",
+            borderRadius: "5px"
+          }}
+        />
+
+        <button
+          style={{
+            width: "100%",
+            padding: "10px",
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer"
+          }}
+        >
+          Register
+        </button>
+
+      </div>
     </div>
   );
 }
